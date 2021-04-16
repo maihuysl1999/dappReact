a356b539960879b4ae16409bc028704c0df268ee
maihuy <maihuysl1999@gmail.com>
2021-04-16 21:28:04 +0700

    create round

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import BoNhaCai from "../contracts/BoNhaCai.json";
-import BoNhaCai from "../contracts/SimpleLottery.json";
+import SimpleLottery from "../contracts/SimpleLottery.json";
 import getWeb3 from "../getWeb3";
 
 class Test extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null, round: roundId};
+  state = {
+    storageValue: 0,
+    web3: null, accounts: null,
+    contract: null, round: null,
+    networkId: 0,
+    numbToken: null,
+  };
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
@@ -14,8 +20,8 @@ class Test extends Component {
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = BoNhaCai.networks[networkId];
+      const netId = await web3.eth.net.getId();
+      const deployedNetwork = BoNhaCai.networks[netId];
       const instance = new web3.eth.Contract(
         BoNhaCai.abi,
         deployedNetwork && deployedNetwork.address,
@@ -24,7 +30,7 @@ class Test extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
+      this.setState({ storageValue: response, web3, accounts, contract: instance, networkId: netId });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -34,44 +40,51 @@ class Test extends Component {
     }
   };
 
-  onBuy = async () => {
-    const { web3, accounts, contract } = this.state;
+  onBuyToken = (e) => {
 
+    const { web3, accounts, contract, numbToken } = this.state;
+    console.log(numbToken);
+
+    // Stores a given value, 5 by default.
+    contract.methods.buyToken().send({ from: accounts[0], value: web3.utils.toWei(numbToken, "ether") });
+
+    // Get the value from the contract to prove it worked.
+    contract.methods.balanceOf(accounts[0]).call().then((res) => {
+      // Update state with the result.
+      this.setState({ storageValue: res });
+    });
+    e.preventDefault();
+  };
+
+  onChange = (e) => {
+    this.setState({
+      numbToken: e.target.value
+    })
+  }
+
+
+  onCreateRound = async () => {
+    const { web3, accounts, contract , networkId} = this.state;
+    const deployedNetwork = SimpleLottery.networks[networkId];
     const instance = new web3.eth.Contract(
       SimpleLottery.abi,
       deployedNetwork && deployedNetwork.address,
     );
+    console.log(instance._address);
 
-    // Stores a given value, 5 by default.
-    await contract.methods.buyToken().send({ from: accounts[0], value: web3.utils.toWei("0.01", "ether") });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.balanceOf(accounts[0]).call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+    await contract.methods.createRound(0, instance._address ,1618479028, 20).send({ from: accounts[0] });
+    window.location.reload();
   };
-
-  // onBuy = async () => {
-  //   const { web3, accounts, contract } = this.state;
-
-  //   // Stores a given value, 5 by default.
-  //   await contract.methods.createRound(0, , ).send({ from: accounts[0]});
-
-  //   // Get the value from the contract to prove it worked.
-  //   const response = await contract.methods.balanceOf(accounts[0]).call();
-
-  //   // Update state with the result.
-  //   this.setState({ storageValue: response });
-  // };
 
   render() {
     return (
       <div className="App">
         <p>{this.state.accounts}</p>
         <h1>Hello</h1>
-        <button onClick={this.onBuyToken}>Buy Token</button>
-        <button onClick={this.onCreateRound}>Buy </button>
+        <form onSubmit={this.onBuyToken}>
+          <input onChange={this.onChange}></input>
+        </form>
+        <button onClick={this.onCreateRound}>createRound</button>
         <p>{this.state.storageValue}</p>
       </div>
     );
