39179344a1be9263bcacc1d4d8442a44d4f3a2ee
maihuy <maihuysl1999@gmail.com>
2021-04-17 22:54:16 +0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -8,50 +8,14 @@ import RouletteWheel from "./components/RouleteWheel";
 import Test from "./components/Test";
 import BettingPanel from "./components/BettingPanel/BettingPanel";
 import ListRound from "./components/ListRound";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  //Link
+} from "react-router-dom";
 
 function App() {
-  // state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
-  // componentDidMount = async () => {
-  //   try {
-  //     // Get network provider and web3 instance.
-  //     const web3 = await getWeb3();
-
-  //     // Use web3 to get the user's accounts.
-  //     const accounts = await web3.eth.getAccounts();
-
-  //     // Get the contract instance.
-  //     const networkId = await web3.eth.net.getId();
-  //     // const deployedNetwork = SimpleStorageContract.networks[networkId];
-  //     // const instance = new web3.eth.Contract(
-  //     //   SimpleStorageContract.abi,
-  //     //   deployedNetwork && deployedNetwork.address,
-  //     // );
-
-  //     // Set web3, accounts, and contract to the state, and then proceed with an
-  //     // example of interacting with the contract's methods.
-  //     this.setState({ web3, accounts});
-  //   } catch (error) {
-  //     // Catch any errors for any of the above operations.
-  //     alert(
-  //       `Failed to load web3, accounts, or contract. Check console for details.`,
-  //     );
-  //     console.error(error);
-  //   }
-  // };
-
-  // runExample = async () => {
-  //   const { accounts, contract } = this.state;
-
-  //   // Stores a given value, 5 by default.
-  //   await contract.methods.set(5).send({ from: accounts[0] });
-
-  //   // Get the value from the contract to prove it worked.
-  //   const response = await contract.methods.get().call();
-
-  //   // Update state with the result.
-  //   this.setState({ storageValue: response });
-  // };
 
   const spin = () => {
     childRef.current.handleSpinClick();
@@ -59,7 +23,7 @@ function App() {
 
   const childRef = useRef();
 
-  return (
+  return ( <Router>
     <div className="App">
       <div
         style={{
@@ -71,7 +35,13 @@ function App() {
         <RouletteWheel ref={childRef} />
         <BettingPanel spin={spin} />
       </div>
+      <Switch>
+          <Route exact path="/SimpleLottery">
+            <Test />
+          </Route>
+        </Switch>
     </div>
+    </Router>  
   );
 }
 
