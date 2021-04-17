fa33b2e1c8a0e8d179b033401fbcaae4e29d9f55
Ubuntu <kun_duc123@yahoo.com.vn>
2021-04-17 23:05:18 +0700

    Add Roulette migrate

--- migrations/3_deploy_nhacai.js
+++ migrations/3_deploy_nhacai.js
@@ -1,9 +1,13 @@
 var web3 = require("web3");
 const BoNhaCai = artifacts.require("./BoNhaCai.sol");
 const SimpleLottery = artifacts.require("./SimpleLottery.sol");
+const Roulette = artifacts.require("./Roulette.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(BoNhaCai, web3.utils.toWei("0.01", "ether")).then(function(){
     return deployer.deploy(SimpleLottery, BoNhaCai.address)
+    .then(function(){
+      return deployer.deploy(Roulette, BoNhaCai.address);
+    })
   });
 };
