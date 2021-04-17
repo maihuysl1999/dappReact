d996f15a6444a78bb246157853c5290c4d4a749f
Ubuntu <kun_duc123@yahoo.com.vn>
2021-04-17 23:17:18 +0700

    small changes

--- client/src/components/Round.js
+++ client/src/components/Round.js
@@ -15,17 +15,6 @@ class Round extends Component {
               Argon is a great free UI package based on Bootstrap 4 that
               includes the most important components and features.
             </p>
-            <div>
-              <Badge color="primary" pill className="mr-1">
-                design
-              </Badge>
-              <Badge color="primary" pill className="mr-1">
-                system
-              </Badge>
-              <Badge color="primary" pill className="mr-1">
-                creative
-              </Badge>
-            </div>
             <Button
               className="mt-4"
               color="primary"
